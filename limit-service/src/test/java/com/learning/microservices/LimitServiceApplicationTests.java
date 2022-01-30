32dcd7341a7aa3eb7aa20440297a6ac383788e04
unknown <shingerohini712@gmail.com>
2022-01-30 14:40:30 +0530

    Added limit project

---
+++ limit-service/src/test/java/com/learning/microservices/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
