720f3d46586e12ea0bc51042785013413c155e83
unknown <shingerohini712@gmail.com>
2022-01-30 19:06:13 +0530

    Added Spring Cloud Config Server

---
+++ spring-cloud-config-server/src/test/java/com/learning/microservice/configserver/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.microservice.configserver.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
