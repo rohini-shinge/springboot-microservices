ff18621968b61c4494b9c03faef91d9a8752a95f
Avani Shinge <shingerohini712@gmail.com>
2022-02-01 16:02:19 +0530

    Added new API for exchange

--- currency-exchange/src/main/java/com/learning/microservice/currencyexchange/controller/CurrencyExchangeController.java
+++ currency-exchange/src/main/java/com/learning/microservice/currencyexchange/controller/CurrencyExchangeController.java
@@ -1,16 +1,27 @@
 package com.learning.microservice.currencyexchange.controller;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.learning.microservice.currencyexchange.entity.CurrencyExchange;
+
 @RestController
 public class CurrencyExchangeController {
 	
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
-	public CurrencyExchange retrieveExchangeValue(@PathVariable)
+	public CurrencyExchange retrieveExchangeValue(@PathVariable String from,@PathVariable String to)
 	{
-		
+		CurrencyExchange currencyExchange= new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+		currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
+		return currencyExchange;
 	}
 
 }
