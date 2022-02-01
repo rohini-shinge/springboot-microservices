b2cadf39a1cb6e09d58420d5a3ded49c5c2b9265
Avani Shinge <shingerohini712@gmail.com>
2022-02-01 18:40:53 +0530

    Invoking exchange service from conversion

--- currency-conversion/src/main/java/com/learning/microservice/currencyconversion/controller/CurrencyConversionController.java
+++ currency-conversion/src/main/java/com/learning/microservice/currencyconversion/controller/CurrencyConversionController.java
@@ -1,20 +1,38 @@
 package com.learning.microservice.currencyconversion.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.learning.microservice.currencyconversion.entity.CurrencyConversion;
 
 @RestController
 public class CurrencyConversionController {
 	
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,@PathVariable String to,@PathVariable BigDecimal quantity) {
 	
-		return new CurrencyConversion(1000L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "8080");
+		Map<String, String> uriVariables=new HashMap<String, String>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		
+		ResponseEntity<CurrencyConversion>  responseEntity=new RestTemplate().
+				getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class,uriVariables);
+		
+		CurrencyConversion currencyConversion=responseEntity.getBody();
+		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, currencyConversion.getConversionMultiple(),
+				quantity.multiply(currencyConversion.getConversionMultiple()), environment.getProperty("local.server.port"));
 	}
 
 }
