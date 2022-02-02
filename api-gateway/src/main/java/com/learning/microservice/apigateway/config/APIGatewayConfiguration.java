6fefcb0613be181b1c6962576aac0afa05eeb551
Avani Shinge <shingerohini712@gmail.com>
2022-02-03 00:02:32 +0530

    Configs moved in different package

--- api-gateway/src/main/java/com/learning/microservice/apigateway/config/APIGatewayConfiguration.java
+++ api-gateway/src/main/java/com/learning/microservice/apigateway/config/APIGatewayConfiguration.java
@@ -1,4 +1,4 @@
-package com.learning.microservice.apigateway;
+package com.learning.microservice.apigateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
